aeb54c0109c9ccf24a1e8796a29e6d840036fbcf
suhanisoni2601 <suhanisoni2601@gmail.com>
2026-07-12 22:50:38 +0530

    Add updatesucess.php for password update confirmation

---
+++ 25ESKCA225/DAY12/updatesucess.php
@@ -0,0 +1,14 @@
+<?php
+include("header2.php");
+?>
+
+<div class="conntainer mt-5 text-center">
+    <div class="alert alert-success">
+        <h3>Update Password Successful</h3>
+</div>
+
+<a href="login.php" class="btn btn-primary">Go To Login</a>
+</div>
+<?php
+include("footer.php");
+?>
