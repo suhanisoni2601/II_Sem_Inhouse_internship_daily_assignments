85b3f2c01a3a43e2274380fde25d637fe1e686bc
suhanisoni2601 <suhanisoni2601@gmail.com>
2026-07-12 22:53:36 +0530

    Create dashboardverticalcontent.php

---
+++ 25ESKCA225/DAY12/dashboardverticalcontent.php
@@ -0,0 +1,13 @@
+<div class="container-fluid">
+    <div class="row">
+    <div class="col-md-3">
+
+    <a href="updatePassword.php">Update Password</a>
+
+    <br>
+
+    <a href="updateprofile.php">Update Profile</a>
+
+    </div>
+
+    <div class="col-md-9">
