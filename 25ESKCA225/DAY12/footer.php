05f350a9539d38e0bddb1e559505168f3a63df80
suhanisoni2601 <suhanisoni2601@gmail.com>
2026-07-12 22:37:15 +0530

    Create footer.php for features and team display
    Add footer.php with features section and styling.

---
+++ 25ESKCA225/DAY12/footer.php
@@ -0,0 +1,120 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="bootstrap.min.css" rel="stylesheet">
+    <style>
+
+        .about{
+            
+            padding:40px 0;
+            background-color:#A8B4A5;
+        }
+        .feature-container{
+            display:flex;
+            flex-direction:row;
+            align-items:center;
+            justify-content:center;
+            margin-top:0px;
+            flex-wrap:nowrap;
+            gap:10px;
+            padding:20px;
+            width:100%;
+            
+            
+
+        }
+
+        .feature{
+            display:flex;
+            flex-direction:column;
+            align-items:center;
+            justify-content:center;
+            padding:20px;
+            width:300px;
+            height:350px;
+            border-radius:10px;
+            box-shadow:0 0 10px rgba(0,0,0,0.1);
+            gap:10px;
+            margin-left:50px;
+            border:1px solid #111010;
+            box-sizing:border-box;
+            background-color:#ede8e7;
+            
+        }
+
+        .img{
+           width:100px;
+           height:100px;
+    
+        }
+
+        h3{
+          font-size:30px;
+          color:#010e10;
+   
+          font-weight:bolder;
+          margin-top:0;
+        }
+
+        body{
+          background-color:#A37774;
+        }
+
+        .our{
+          text-align:center;
+          margin-top:30px;
+          font-size:60px;
+          color:#000601;
+          font-weight:bolder;
+        }
+
+        
+        </style>
+
+
+
+    
+</head>
+<body>
+    
+
+<section class="about" id="about">
+
+<div class="container">
+
+<h1 class="our">OUR FEATURES</h1>
+<br>
+<br>
+<p class="text-center fs-5" style="font-size:22px;">
+
+Industrial Training Portal is designed to help students gain
+practical experience through industrial training, live projects,
+technical workshops and mentorship. Our aim is to prepare students
+for internships and placements with industry-relevant skills.
+
+</p>
+<br>
+
+</div>
+ 
+ <br>
+    <div class="feature-container">
+        <div class="feature"><h3>OUR TEAM</h3>
+            <img  class="img" src="https://img.magnific.com/free-photo/businesspeople-having-good-time-meeting_1098-1786.jpg?semt=ais_hybrid&w=740&q=80">
+            <p>We have a team of highly skilled professionals who are dedicated to providing the best service to our clients.</p>   
+        </div>
+        <div class="feature"><h3>RESULTS</h3>
+        <img class="img" src="https://img.magnific.com/premium-vector/green-tick-green-arrow-pointing-right_1134661-13946.jpg?semt=ais_hybrid&w=740&q=80">
+    <p>our results have proven to be excellent in each and every field.</p></div>
+    <div class="feature"><h3>24/7 SUPPORT</h3>
+    <img class="img" src="https://static.vecteezy.com/system/resources/previews/024/254/497/non_2x/illustration-of-handshake-icon-vector.jpg">
+<p>We offer 24/7 support to ensure your satisfaction and resolve any issues promptly.</p></div>
+
+    </div>
+
+</section>
+</body>
+</html>
