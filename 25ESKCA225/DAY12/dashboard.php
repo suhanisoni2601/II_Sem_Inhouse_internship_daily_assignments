4bceabeb952050dd8ce788b4af3f8629775956c9
suhanisoni2601 <suhanisoni2601@gmail.com>
2026-07-12 22:43:58 +0530

    Add dashboard.php for user dashboard view
    Create dashboard.php to display user dashboard

---
+++ 25ESKCA225/DAY12/dashboard.php
@@ -0,0 +1,15 @@
+<?php
+ include("header2.php");
+include("dashboardverticalcontent.php");
+
+?>
+
+    
+<h2> <?php echo "Welcome,". $_SESSION['user_name'] . "!";
+
+?></h2>
+
+<?php
+include("dashboardfooter.php");
+include("footer.php");
+ ?>
